6d8c9b9d8d73242c55ae376d1e33021daaa45351
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-30 00:52:53 +0300

    static routes

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`serving on port ${PORT}`));
