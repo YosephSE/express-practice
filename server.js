5f6ed252c3946559aa91b50557e0961c86c5d8f9
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-30 00:50:29 +0300

    smt

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+const path = require('path')
 const app = express();
 const PORT = process.env.PORT;
 
-
-
-
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
 
 app.listen(PORT, () => console.log(`serving on port ${PORT}`));
